7a54f00c8fb859096368f1efd68f740cb50aa5e1
Andrey Murilev <94217327+AndySter@users.noreply.github.com>
2022-02-13 23:07:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while(a!=b){
-        if(a>b){
-            a=a-b;
+    while (a != b) {
+        if (a > b) {
+            a = a-b;
         }
-        else{
-            b=b-a;
+        else {
+            b = b-a;
         }
     }
     return a;
